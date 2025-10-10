66e03d8d6744dc7d20222766a4e1f8a9c082055c
lukinaolha <lukina.olha@lll.kpi.ua>
2025-10-10 20:45:21 +0300

    main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "GameManager.h"
+
+int main() {
+    GameManager game(10, 10);
+    game.init();
+
+    while (game.isRunning()) {
+        game.render();  
+        game.update(); 
+    }
+
+    cout << "\nGame Over!\n";
+    return 0;
+}
